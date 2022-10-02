148b62e317b1a9c2cf97c9174f7364b0982f69d8
David Biersach <dbiersach@gmail.com>
2022-10-01 23:09:14 -0400

    Change line spacing

--- labs/Session 02 - Arrays and Algorithms/instructor/coprime_probabilty_instructor.py
+++ labs/Session 02 - Arrays and Algorithms/instructor/coprime_probabilty_instructor.py
@@ -9,9 +9,10 @@ num_pairs = 10_000
 
 a = np.random.randint(1, max_num, size=num_pairs)
 b = np.random.randint(1, max_num, size=num_pairs)
-c = np.gcd(a, b)
 
+c = np.gcd(a, b)
 num_coprime_pairs = np.count_nonzero(c == 1)
 p = num_coprime_pairs / num_pairs
+
 print(f"Coprime Probability = {p}")
 print(f"Hidden constant     = {np.sqrt(6 / p)}")
