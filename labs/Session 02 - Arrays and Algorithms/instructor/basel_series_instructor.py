d1bb517fd2f922432103c84b6fb3413ce093374e
David Biersach <dbiersach@gmail.com>
2022-10-17 23:45:41 -0400

    Fix range on Basel series

--- labs/Session 02 - Arrays and Algorithms/instructor/basel_series_instructor.py
+++ labs/Session 02 - Arrays and Algorithms/instructor/basel_series_instructor.py
@@ -4,9 +4,9 @@ import numpy as np
 
 num_terms = 10_000
 
-a = np.arange(1, num_terms)
+a = np.arange(1, num_terms + 1)
 
-sigma = np.sum(1 / a)
+sigma = np.sum(1 / a**2)
 
 print(f"Sum of first {num_terms:,} terms = {sigma}")
 
