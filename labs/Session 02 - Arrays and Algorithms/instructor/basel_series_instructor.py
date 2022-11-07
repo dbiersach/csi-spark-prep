7b6f13b3c799193290bfd9cc6bb4392661f657fb
David Biersach <dbiersach@gmail.com>
2022-11-06 22:44:53 -0500

    Fix range exclusive end

--- labs/Session 02 - Arrays and Algorithms/instructor/basel_series_instructor.py
+++ labs/Session 02 - Arrays and Algorithms/instructor/basel_series_instructor.py
@@ -6,12 +6,12 @@ import numpy as np
 def main():
     num_terms = 10_000
 
-    a = np.arange(1, num_terms)
+    a = np.arange(1, num_terms + 1)
 
     sigma = np.sum(1 / a)
 
     print(f"Sum of first {num_terms:,} terms = {sigma}")
-    
+
     print(f"Magic number = {np.sqrt(sigma * 6)}")
 
 
